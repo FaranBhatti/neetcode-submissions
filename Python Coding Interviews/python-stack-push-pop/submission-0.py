9a5800e3fb387bba2039de07c1b144d9dabcad56
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2026-04-28 09:38:52 -0700

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    # take a list of integers, return a new list of integers in the reverse order
+    # pop() would take an element off of a stack and return that value
+    reversed_list = []
+    
+    while len(arr) > 0:
+        reversed_list.append(arr.pop())
+    
+    return reversed_list
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
