fe4944a52453e80827a72fad636528fa2e8cd109
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2026-05-02 17:26:28 -0700

    Add: python-dict-comprehension - submission-0

---
+++ Python Coding Interviews/python-dict-comprehension/submission-0.py
@@ -0,0 +1,13 @@
+from typing import List, Dict
+
+
+def num_to_index(nums: List[int]) -> Dict[int, int]:
+    # take a list of integers and return a dict. key = elements of the list, value = indices of those elements in the list
+    # can assume all elements in list are unique
+    return {index: num for num, index in enumerate(nums)}
+
+
+# do not modify below this line
+print(num_to_index([1, 2, 3, 4, 5, 6, 7, 8]))
+print(num_to_index([8, 7, 6, 5, 4, 3, 2, 1]))
+print(num_to_index([0, 3, 2, 4, 5, 1]))
