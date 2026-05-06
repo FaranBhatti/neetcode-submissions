631e8adb11d43238e486a019b9b7037b56e1a40d
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2026-05-06 16:13:01 -0700

    Add: python-sorted-dict-basics - submission-0

---
+++ Python Coding Interviews/python-sorted-dict-basics/submission-0.py
@@ -0,0 +1,40 @@
+from typing import List
+from sortedcontainers import SortedDict
+
+
+def remove_keys(sorted_dict: SortedDict[str, int], keys: List[str]) -> SortedDict[str, int]:
+    """
+    have: a sorted dictionary called 'sorted_dict', a list of strings which are keys called 'keys'
+    task: remove key-value pairs associated with thsoe keys from the dict
+    return: return modified sorted dictionary 'sorted_dict'
+    """
+    for key in keys:
+        if key in sorted_dict:
+            sorted_dict.pop(key)
+    
+    return sorted_dict
+
+
+def get_values_before_target(sorted_dict: SortedDict[str, int], target: str) -> List[int]:
+    """
+    have: sorted dictionary called 'sorted_dict', target key called 'target'
+    task: obtain list of values with keys that come before the 'target' key in 'sorted_dict'
+    return: list of values
+    """
+    values = []
+    for key, value in sorted_dict.items():
+        if key != target:
+            values.append(value)
+        else:
+            return values
+
+# do not modify below this line
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), ['Bob']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), ['Bob', 'David']))
+print(remove_keys(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45}), ['Alice', 'Eve']))
+
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'David'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Charlie'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Bob'))
+print(get_values_before_target(SortedDict({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}), 'Alice'))
